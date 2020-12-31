0bd892b6dbeff1d96fa8a542cfa238f939466c9e
TomaszBeben <tbeben@st.swps.edu.pl>
2020-12-31 17:23:51 +0100

    resume in navbar to correct

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,35 +1,35 @@
-import React,{ useState } from 'react'
+// import React,{ useState } from 'react'
 
-import CvPl from '../files/CV_Tomasz_Bęben.pdf'
-import CvEn from '../files/enCV_Tomasz_Beben.pdf'
-import CvImg from '../images/cv.jpg'
-import PlFlag from '../images/plFlag.jpg'
-import EnFlag from '../images/enFlag.jpg'
+// import CvPl from '../files/CV_Tomasz_Bęben.pdf'
+// import CvEn from '../files/enCV_Tomasz_Beben.pdf'
+// import CvImg from '../images/cv.jpg'
+// import PlFlag from '../images/plFlag.jpg'
+// import EnFlag from '../images/enFlag.jpg'
 
-const Resume = () => {
-    const resume = 'resume-img';
-    const hiddenResume = 'resume-img resume-img-clicked';
-    const [cv, setCv] = useState(resume);
-    const switchResume = () => cv === hiddenResume ? setCv(resume) : setCv(hiddenResume);
+// const Resume = () => {
+//     const resume = 'resume-img';
+//     const hiddenResume = 'resume-img resume-img-clicked';
+//     const [cv, setCv] = useState(resume);
+//     const switchResume = () => cv === hiddenResume ? setCv(resume) : setCv(hiddenResume);
 
-    const[flags, setFlags]= useState('')
-    const switchFlags = () => flags === '' ? setFlags('2') : setFlags('');
+//     const[flags, setFlags]= useState('')
+//     const switchFlags = () => flags === '' ? setFlags('2') : setFlags('');
 
-    return (
-        <div className='resume-container'>
-            <div className='resume-img-container'>
-                <img className={cv} src={CvImg} alt="CV" onClick={()=>{switchResume();switchFlags();}} />
-            </div>
-            <div className='resume-flag-container'>
-                <a href={null} download>
-                    <img className={`flag${flags} pl-flag${flags}`} src={PlFlag} alt="PlFlag" onClick={()=>{switchResume();switchFlags();}}/>
-                </a>
-                <a href={null} download>
-                    <img className={`flag${flags} en-flag${flags}`} src={EnFlag} alt="EnFlag" onClick={()=>{switchResume();switchFlags();}}/>
-                </a>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className='resume-container'>
+//             <div className='resume-img-container'>
+//                 <img className={cv} src={CvImg} alt="CV" onClick={()=>{switchResume();switchFlags();}} />
+//             </div>
+//             <div className='resume-flag-container'>
+//                 <a href={null} download>
+//                     <img className={`flag${flags} pl-flag${flags}`} src={PlFlag} alt="PlFlag" onClick={()=>{switchResume();switchFlags();}}/>
+//                 </a>
+//                 <a href={null} download>
+//                     <img className={`flag${flags} en-flag${flags}`} src={EnFlag} alt="EnFlag" onClick={()=>{switchResume();switchFlags();}}/>
+//                 </a>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Resume
+// export default Resume
--- src/components/TestArea.js
+++ src/components/TestArea.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-import cv from '../images/cv.jpg'
+import CvPl from '../files/CV_Tomasz_Bęben.pdf'
+import CvEn from '../files/enCV_Tomasz_Beben.pdf'
+import CvImg from '../images/cv.jpg'
+import PlFlag from '../images/plFlag.jpg'
+import EnFlag from '../images/enFlag.jpg'
 
 const TestArea = () => {
+    const [flagEn, setFlagsEn] = useState('hide');
+    const switchFlagEn = () => flagEn === 'hide' ? setFlagsEn(`en-flag`) : setFlagsEn('hide');
+
+    const [flagPl, setFlagPl] = useState('hide')
+    const switchFlagPl = () => flagPl === 'pl-flag' ? setFlagPl(`hide`) : setFlagPl('pl-flag');
     return (
         <div>
-            <img src={cv} alt="cv" className='cv'/>
+            <a href={CvPl} download><img src={PlFlag} alt="cv" className={`${flagPl}`}/></a>
+            <a href={CvEn} download><img src={EnFlag} alt="cv" className={`${flagEn}`}/></a>
+            <img src={CvImg} alt="cv" className='cv' onClick={()=>{switchFlagEn(); switchFlagPl()}}/>
+
         </div>
     )
 }
 
 export default TestArea;
+
+//do poprawy
