dd4a66e3c80b9140dd49806d02adeece30fcf69b
TomaszBeben <tbeben@st.swps.edu.pl>
2021-01-03 22:11:30 +0100

    nothing

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,21 +1,21 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Burger from 'react-css-burger';
 
 const Hamburger = () => {
-    const[active, setActive] = useState(false)//turn to 'true' to disable view
-    const toggle = () => active === false ? setActive(true) : setActive(false);
+  const [active, setActive] = useState(false)
+  const toggle = () => active === false ? setActive(true) : setActive(false);
 
-    return (
-        <Burger
-        onClick={() => setActive(toggle)}
-        active={active}
-        burger="collapse"
-        color="white"
-        marginTop= '0px'
-        marginLeft= '0px'
-        hoverOpacity={0.8}
-      />
-    )
+  return (
+    <Burger
+      onClick={() => setActive(toggle)}
+      active={active}
+      burger="collapse"
+      color="white"
+      marginTop='0px'
+      marginLeft='0px'
+      hoverOpacity={0.8}
+    />
+  )
 }
 
 export default Hamburger;
